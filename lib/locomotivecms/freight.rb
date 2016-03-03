7cbeb4c423b2782a0b867d124d7f9713785a0ab5
Joel Helbling <joel@joelhelbling.com>
2016-03-03 09:33:47 -0500

    Initial commit of generated gem

---
+++ lib/locomotivecms/freight.rb
@@ -0,0 +1,7 @@
+require "locomotivecms/freight/version"
+
+module Locomotivecms
+  module Freight
+    # Your code goes here...
+  end
+end
---
+++ lib/locomotivecms/freight/version.rb
@@ -0,0 +1,5 @@
+module Locomotivecms
+  module Freight
+    VERSION = "0.1.0"
+  end
+end
